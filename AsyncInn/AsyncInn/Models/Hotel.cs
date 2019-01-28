9ef087a68e0f6034fb8ac68429f4e34274635d4b
mbgoseco <mbgoseco@gmail.com>
2019-01-27 17:57:01 -0800

    setting up DB models

---
+++ AsyncInn/AsyncInn/Models/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Hotel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        // Navigation
+        public ICollection<HotelRoom> HotelRooms { get; set; }
+    }
+}
